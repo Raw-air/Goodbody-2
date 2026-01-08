8f8675fe935666444ef25b9f157e330b0ab7bb51
Raw-air <110284697+Raw-air@users.noreply.github.com>
2026-01-08 13:43:08 +0000

    更新

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,8 @@
-// 簡單的 Service Worker，為了符合安裝標準
-self.addEventListener('fetch', function(event) {
-    // 這裡暫時不做任何事
+// 這是讓 Android 認可妳是 APP 的必要檔案
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // 這裡暫不處理快取，確保妳每次打開都是最新版
 });
